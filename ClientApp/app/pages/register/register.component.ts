b6087354d730aadd4951b9cff1d1b1e8e69d087a
JP <paulotteodosio@gmail.com>
2020-04-02 19:19:07 -0300

    remove login after register

--- ClientApp/app/pages/register/register.component.ts
+++ ClientApp/app/pages/register/register.component.ts
@@ -18,6 +18,8 @@ import { DeviceDetectorService } from "../../modules/ngx-device-detector/device-
 import { isPlatformBrowser } from "@angular/common";
 import { environment } from "../../app.environment";
 import { SpinnerService } from "../../services/spinner.service";
+import Swal from "sweetalert2";
+import { PrivateKeysGenerator } from '../register/privatekeys.generator';
 
 @Component({
   selector: "register",
@@ -163,73 +165,28 @@ export class RegisterComponent implements OnInit {
     this.createResponse = await this._userService.createUser(this.userInfo);
 
     if (this.createResponse.isValid) {
-      await this.Login();
+      Swal({
+        type: "success",
+        text: "Your account has been created, confirm to download your Master Security Code",
+        customClass: "animated fadeInDown",
+        showConfirmButton: true,
+        allowOutsideClick: false,
+        heightAuto: false
+      }).then(() => {
+        PrivateKeysGenerator.generate(null, this.userInfo.username, this.userInfo.recoveryKey);
+        this._router.navigate(["/login"]);
+      });
     } else {
       this.userInfo.termsVersion = null!;
     }
 
     Util.setButtonAsReadyState(this.btnSubmit);
-    // this.inProgress = false;
     this.sending = false;
     this.sendButtonText = this.sendText;
   }
 
-  async Login() {
-    let token = new TokenRequest();
-    token.username = this.userInfo.username;
-    token.password = this.userInfo.password;
-    token.responseRecaptcha = this.userInfo.responseRecaptcha;
-
-    await this._userService.getUserToken(token);
-
-    if (this._shared.isTokenValid) {
-      await this._userService.getUser(token);
-
-      await this._wallet.getWallet();
-
-      await this.setIsAuthenticated();
-
-      let exportResponse = await this._wallet.exportWallet({
-        data: this._shared.recoveryKey.recoveryKey,
-        userkey: token.password
-      });
-
-      if (Util.isValidObject(exportResponse) && exportResponse.isValid) {
-        await this._shared.exportPrivateKeys(
-          exportResponse,
-          token.username,
-          this._shared.recoveryKey.recoveryKey
-        );
-      }
-
-      let redirect = confirm(
-        "Did you save your Master Security Code and your Private Key? Can we redirect you?"
-      );
-
-      if (redirect) {
-        this.redirectIfAuthenticated();
-      } else {
-        this.showInfoPanel = true;
-      }
-    }
-  }
-
-  async securityCodeConfirm() {
-    this.inProgress = true;
-  }
-
-  redirectIfAuthenticated() {
-    if (this._shared.isAuthenticated) {
-      this._router.navigate(["/overview"]);
-    }
-  }
-
   resolved(captchaResponse: string) {
     this.userInfo.responseRecaptcha = captchaResponse;
-  }
-
-  private async setIsAuthenticated() {
-    this._shared.dataStore.isAuthenticated = this._shared.isTokenValid;
   }
 
   goToTop() {
